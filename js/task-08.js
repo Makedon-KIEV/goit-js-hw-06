73e99c60be68e2aefcbfdd9dd91a338bc01beed4
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-10-30 13:40:17 +0300

    task 2
    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
 
+<form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form>
