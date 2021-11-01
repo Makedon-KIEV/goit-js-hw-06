28a4946189905b9b820f4a711e68e0fa8afd7d56
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-11-01 12:06:51 +0200

    task 8
    done

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,25 @@
+const refs = { form: document.querySelector('.login-form') };
 
-<form class="login-form">
-  <label>
-    Email
-    <input type="email" name="email" />
-  </label>
-  <label>
-    Password
-    <input type="password" name="password" />
-  </label>
-  <button type="submit">Login</button>
-</form>
+const onSubmitForm = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const inputData = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  if (email.value === '' || password.value === '') {
+    event.currentTarget.reset();
+    return alert('All fields must be completed!');
+  }
+
+  console.log('Input Data: ', inputData);
+
+  event.currentTarget.reset();
+};
+
+refs.form.addEventListener('submit', onSubmitForm);
